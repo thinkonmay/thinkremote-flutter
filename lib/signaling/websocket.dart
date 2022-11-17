376bac5de38752d9042ca905a3ec6a7ecae14b6a
epitchi <thienvanlea1@gmail.com>
2022-11-17 16:52:28 +0700

    Add Log

--- lib/signaling/websocket.dart
+++ lib/signaling/websocket.dart
@@ -1,89 +1,72 @@
-
-import 'dart:convert';
 import 'dart:html';
 
 import 'package:flutter_webrtc_client/model/signaling.model.dart';
-import 'package:flutter_webrtc_client/signaling/websocket_web.dart';
+import 'package:flutter_webrtc_client/utils/log.dart';
 
 typedef PacketHandlerType = void Function({Map<String, String> Data});
-class SignallingClient
-{
+
+class SignallingClient {
   late WebSocket WebSocketConnection;
   late PacketHandlerType PacketHandler;
 
-  SignallingClient(
-    String url,
-    String token,
-    PacketHandlerType PacketHandler){
-      PacketHandler = PacketHandler;
-        // LogConnectionEvent(ConnectionEvent.WebSocketConnecting)
-        WebSocketConnection = WebSocket("$url?token=$token");
-        WebSocketConnection.onOpen.listen((Event e) => onServerOpen(e));
-    }
+  SignallingClient(String url, String token, PacketHandlerType PacketHandler) {
+    PacketHandler = PacketHandler;
+    LogConnectionEvent(ConnectionEvent.WebSocketConnecting);
+    WebSocketConnection = WebSocket("$url?token=$token");
+    WebSocketConnection.onOpen.listen((Event e) => onServerOpen(e));
+  }
 
-    /*
+  /*
      * Fired whenever the signalling websocket is opened.
      * Sends the peer id to the signalling server.
      */
-    onServerOpen(Event event)
-    {
-        // LogConnectionEvent(ConnectionEvent.WebSocketConnected)
-        WebSocketConnection.onError.listen((Event e) { 
-            // Log(LogLevel.Error,`websocket connection error : ${eve.type}`)
-            onServerError();
-        });
+  onServerOpen(Event event) {
+    LogConnectionEvent(ConnectionEvent.WebSocketConnected);
+    WebSocketConnection.onError.listen((Event e) {
+      Log(LogLevel.Error, "websocket connection error : ${e.type}");
+      onServerError();
+    });
 
-        WebSocketConnection.onMessage.listen((MessageEvent e) { 
-          onServerMessage(e);
-        });
+    WebSocketConnection.onMessage.listen((MessageEvent e) {
+      onServerMessage(e);
+    });
 
-        WebSocketConnection.onClose.listen((Event e) {
-            // Log(LogLevel.Error,`websocket connection closed : ${eve.type}`)
-            onServerError();
-        });
-    }
+    WebSocketConnection.onClose.listen((Event e) {
+      Log(LogLevel.Error, "websocket connection closed : ${e.type}");
+      onServerError();
+    });
+  }
 
-    /*
+  /*
      * send messsage to signalling server
      * @param {string} request_type 
      * @param {any} content 
      */
-    SignallingSend(String Target, Map<String, String> Data)
-    {
-        var dat =  UserRequest(0,
-                Target,
-                <String, String>{},
-                Data).toString();
-        // Log(LogLevel.Debug,`sending message : ${dat}`);
-        WebSocketConnection.send(dat);
-    }
+  SignallingSend(String Target, Map<String, String> Data) {
+    var dat = UserRequest(0, Target, <String, String>{}, Data).toString();
+    Log(LogLevel.Debug, "sending message : $dat");
+    WebSocketConnection.send(dat);
+  }
 
-    /*
+  /*
      * Fired whenever the signalling websocket emits and error.
      * Reconnects after 3 seconds.
      */
-    onServerError() 
-    {
-        // Log(LogLevel.Warning,"websocket connection disconnected");
-        // LogConnectionEvent(ConnectionEvent.WebSocketDisconnected)
-    }
+  onServerError() {
+    Log(LogLevel.Warning, "websocket connection disconnected");
+    LogConnectionEvent(ConnectionEvent.WebSocketDisconnected);
+  }
 
-
-    /*
+  /*
      * handle message from signalling server during connection handshake
      * @param {Event} event 
      * @returns 
      */
-    onServerMessage(MessageEvent event) 
-    {
-        var response = UserResponse(event.data.id,
-                                        event.data.error,
-                                        event.data.data);
+  onServerMessage(MessageEvent event) {
+    var response =
+        UserResponse(event.data.id, event.data.error, event.data.data);
 
-        // Log(LogLevel.Debug,`received signaling message: ${response.toString()}`);
-        PacketHandler(Data: response.Data);
-    }
+    Log(LogLevel.Debug, "received signaling message: ${response.toString()}");
+    PacketHandler(Data: response.Data);
+  }
 }
-
-
-
